a2e9423fac6f4f94fea7aa12ddf63f3727d748b7
cosmin bucur <cosmin.bucur@mambu.com>
2022-11-14 15:29:12 +0200

    ER8 add jdbc and hibernate WIP

---
+++ hibernate/src/main/java/com/sda/hibernate/states/DemoHibernateStates.java
@@ -0,0 +1,29 @@
+package com.sda.hibernate.states;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.sda.hibernate.config.HibernateUtil;
+import com.sda.hibernate.crud.Person;
+
+public class DemoHibernateStates {
+
+	public static void main(String[] args) {
+
+		Person person = new Person();
+		person.setFirstName("jon");
+		person.setLastName("snow");
+		person.setEmail("jonsnow@gmail.com");
+		person.setCountry("westeros");
+		// Transient object state
+
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction transaction = session.beginTransaction();
+		session.save(person);
+		transaction.commit();
+		// Persistent object state
+
+		session.close();
+		// Detached object state
+	}
+}
